c26ebd7285654df1718d160616cd13aab8bd0ec7
K4mD4m <KamyszekDamian@outlook.com>
2026-01-09 13:04:14 +0100

    Refactor for browser contexts

--- src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
+++ src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
@@ -1,43 +1,49 @@
 package com.serenitydojo.playwright;
 
-import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.BrowserType;
-import com.microsoft.playwright.Page;
-import com.microsoft.playwright.Playwright;
-import com.microsoft.playwright.junit.Options;
-import com.microsoft.playwright.junit.OptionsFactory;
-import com.microsoft.playwright.junit.UsePlaywright;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import com.microsoft.playwright.*;
+import org.junit.jupiter.api.*;
 
 import java.util.Arrays;
 
-@UsePlaywright(ASimplePlaywrightTest.MyOptions.class)
 public class ASimplePlaywrightTest {
 
-    public static class MyOptions implements OptionsFactory {
+    private static Playwright playwright;
+    private static Browser browser;
+    private static BrowserContext browserContext;
 
-        @Override
-        public Options getOptions(){
-            return new Options()
-                    .setHeadless(false)
-                    .setLaunchOptions(
-                            new BrowserType.LaunchOptions()
-                                    .setArgs(Arrays.asList("--no-sanbox", "--disable-gpu"))
-                    );
-        }
+    Page page;
+
+    @BeforeAll
+    public static void setUpBrowser() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(
+                new BrowserType.LaunchOptions()
+                        .setHeadless(false)
+                        .setArgs(Arrays.asList("--no-sandbox", "--disable-extensions", "--disable-gpu"))
+        );
+        browserContext = browser.newContext();
     }
+
+    @BeforeEach
+    public void setUp() {
+        page = browserContext.newPage();
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        browser.close();
+        playwright.close();
+    }
+
     @Test
-    void shouldShowThePageTitle(Page page) {
+    void shouldShowThePageTitle() {
         page.navigate("https://www.practicesoftwaretesting.com/");
         String title = page.title();
         Assertions.assertTrue(title.contains("Practice Software Testing"));
     }
 
     @Test
-    void shouldSearchByKeyword(Page page) {
+    void shouldSearchByKeyword() {
         page.navigate("https://www.practicesoftwaretesting.com/");
         page.locator("[placeholder=Search]").fill("Pilers");
         page.locator("button:has-text('Search')").click();
